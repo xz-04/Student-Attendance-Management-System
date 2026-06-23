8e03e3d467777ed7b5fed4c5c7c365174733f1e3
xz-04 <1121iamme@gmail.com>
2026-06-23 12:56:55 +0800

    sql

--- studentattendancemanagementsystem.sql
+++ studentattendancemanagementsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 23, 2026 at 06:37 AM
+-- Generation Time: Jun 23, 2026 at 06:56 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -190,7 +190,16 @@ CREATE TABLE `lecturer` (
 --
 
 INSERT INTO `lecturer` (`matricNo`) VALUES
-('l001');
+('L001'),
+('L002'),
+('L003'),
+('L004'),
+('L005'),
+('L006'),
+('L007'),
+('L008'),
+('L009'),
+('L010');
 
 -- --------------------------------------------------------
 
@@ -243,7 +252,21 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`matricNo`, `programmeId`, `intakeDate`) VALUES
-('s001', 'CS-SE', '2025-09-17');
+('S001', 'CS-SE', '2025-09-17'),
+('S002', 'CS-SE', '2025-09-17'),
+('S003', 'CS-SE', '2025-09-17'),
+('S004', 'CS-SE', '2025-09-17'),
+('S005', 'CS-IM', '2026-02-15'),
+('S006', 'CS-SE', '2025-09-17'),
+('S007', 'CS-SE', '2025-09-17'),
+('S008', 'CS-MC', '2025-09-17'),
+('S009', 'CS-MC', '2025-09-17'),
+('S010', 'CS-IM', '2025-09-17'),
+('S011', 'CS-IM', '2025-09-17'),
+('S012', 'CS-SE', '2026-02-15'),
+('S013', 'CS-SE', '2026-02-15'),
+('S014', 'CS-MC', '2026-02-15'),
+('S015', 'CS-MC', '2026-02-15');
 
 --
 -- Triggers `student`
@@ -280,10 +303,80 @@ CREATE TABLE `studentcourse` (
 --
 
 INSERT INTO `studentcourse` (`matricNo`, `courseCode`) VALUES
-('s001', 'CSE3203'),
-('s001', 'CSF3043'),
-('s001', 'CSF3213'),
-('s001', 'MTK3053');
+('S001', 'CSE3203'),
+('S001', 'CSF3043'),
+('S001', 'CSF3213'),
+('S001', 'MTK3053'),
+('S002', 'CSF3003'),
+('S002', 'CSF3023'),
+('S002', 'CSF3034'),
+('S002', 'CSF3143'),
+('S002', 'CSF3243'),
+('S003', 'CSF3003'),
+('S003', 'CSF3023'),
+('S003', 'CSF3034'),
+('S003', 'CSF3143'),
+('S003', 'CSF3243'),
+('S004', 'CSF3003'),
+('S004', 'CSF3023'),
+('S004', 'CSF3034'),
+('S004', 'CSF3143'),
+('S004', 'CSF3243'),
+('S005', 'CSF3003'),
+('S005', 'CSF3023'),
+('S005', 'CSF3034'),
+('S005', 'CSF3143'),
+('S005', 'CSF3243'),
+('S006', 'CSF3003'),
+('S006', 'CSF3023'),
+('S006', 'CSF3034'),
+('S006', 'CSF3143'),
+('S006', 'CSF3243'),
+('S007', 'CSF3003'),
+('S007', 'CSF3023'),
+('S007', 'CSF3034'),
+('S007', 'CSF3143'),
+('S007', 'CSF3243'),
+('S008', 'CSF3003'),
+('S008', 'CSF3023'),
+('S008', 'CSF3034'),
+('S008', 'CSF3143'),
+('S008', 'CSF3243'),
+('S009', 'CSF3003'),
+('S009', 'CSF3023'),
+('S009', 'CSF3034'),
+('S009', 'CSF3143'),
+('S009', 'CSF3243'),
+('S010', 'CSF3003'),
+('S010', 'CSF3023'),
+('S010', 'CSF3034'),
+('S010', 'CSF3143'),
+('S010', 'CSF3243'),
+('S011', 'CSF3003'),
+('S011', 'CSF3023'),
+('S011', 'CSF3034'),
+('S011', 'CSF3143'),
+('S011', 'CSF3243'),
+('S012', 'CSF3003'),
+('S012', 'CSF3023'),
+('S012', 'CSF3034'),
+('S012', 'CSF3143'),
+('S012', 'CSF3243'),
+('S013', 'CSF3003'),
+('S013', 'CSF3023'),
+('S013', 'CSF3034'),
+('S013', 'CSF3143'),
+('S013', 'CSF3243'),
+('S014', 'CSF3003'),
+('S014', 'CSF3023'),
+('S014', 'CSF3034'),
+('S014', 'CSF3143'),
+('S014', 'CSF3243'),
+('S015', 'CSF3003'),
+('S015', 'CSF3023'),
+('S015', 'CSF3034'),
+('S015', 'CSF3143'),
+('S015', 'CSF3243');
 
 -- --------------------------------------------------------
 
@@ -327,7 +420,30 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`matricNo`, `password`, `fullName`, `role`, `facultyName`, `email`, `phoneNo`, `profilePhoto`) VALUES
 ('A001', '123', 'Admin', 'Admin', 'PPPA', 'admin@edu.my', NULL, NULL),
 ('L001', '123', 'Dr. Smith', 'lecturer', 'FSKM', 'smith@university.edu', NULL, NULL),
-('S001', '123', 'Asif', 'student', 'FSKM', 'asif@student.edu', NULL, NULL);
+('L002', '123', 'Dr. Ariff', 'lecturer', 'FSKM', 'ariff@university.edu', NULL, NULL),
+('L003', '123', 'Dr. Siti', 'lecturer', 'FSKM', 'siti@university.edu', NULL, NULL),
+('L004', '123', 'Dr. Razak', 'lecturer', 'FSKM', 'razak@university.edu', NULL, NULL),
+('L005', '123', 'Dr. Hidayah', 'lecturer', 'FSKM', 'hidayah@university.edu', NULL, NULL),
+('L006', '123', 'Dr. Tan', 'lecturer', 'FSKM', 'tan@university.edu', NULL, NULL),
+('L007', '123', 'Dr. Wong', 'lecturer', 'FSKM', 'wong@university.edu', NULL, NULL),
+('L008', '123', 'Dr. Zaki', 'lecturer', 'FSKM', 'zaki@university.edu', NULL, NULL),
+('L009', '123', 'Dr. Yusof', 'lecturer', 'FSKM', 'yusof@university.edu', NULL, NULL),
+('L010', '123', 'Dr. Rahim', 'lecturer', 'FSKM', 'rahim@university.edu', NULL, NULL),
+('S001', '123', 'Asif', 'student', 'FSKM', 'asif@student.edu', NULL, NULL),
+('S002', '123', 'Budi', 'student', 'FSKM', 'budi@student.edu', NULL, NULL),
+('S003', '123', 'Chia', 'student', 'FSKM', 'chia@student.edu', NULL, NULL),
+('S004', '123', 'Danial', 'student', 'FSKM', 'danial@student.edu', NULL, NULL),
+('S005', '123', 'Ezzah', 'student', 'FSKM', 'ezzah@student.edu', NULL, NULL),
+('S006', '123', 'Farah', 'student', 'FSKM', 'farah@student.edu', NULL, NULL),
+('S007', '123', 'Gopal', 'student', 'FSKM', 'gopal@student.edu', NULL, NULL),
+('S008', '123', 'Hana', 'student', 'FSKM', 'hana@student.edu', NULL, NULL),
+('S009', '123', 'Irfan', 'student', 'FSKM', 'irfan@student.edu', NULL, NULL),
+('S010', '123', 'Jasmine', 'student', 'FSKM', 'jasmine@student.edu', NULL, NULL),
+('S011', '123', 'Kumar', 'student', 'FSKM', 'kumar@student.edu', NULL, NULL),
+('S012', '123', 'Lina', 'student', 'FSKM', 'lina@student.edu', NULL, NULL),
+('S013', '123', 'Maya', 'student', 'FSKM', 'maya@student.edu', NULL, NULL),
+('S014', '123', 'Nizam', 'student', 'FSKM', 'nizam@student.edu', NULL, NULL),
+('S015', '123', 'Olivia', 'student', 'FSKM', 'olivia@student.edu', NULL, NULL);
 
 -- --------------------------------------------------------
 
